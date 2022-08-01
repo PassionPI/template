8e18930f0f44669f5bdf514b0d388e26ccea5eca
land.pan <land.pan@momenta.ai>
2022-08-01 20:26:28 +0800

    deno

--- deno/src/utils/resolve.ts
+++ deno/src/utils/resolve.ts
@@ -1,14 +1,5 @@
 export function resolve(meta: ImportMeta, path: string): string {
-  const url = meta.url.split("/").slice(0, -1);
-  const dir = path.split("/");
-  for (let i = 0; i < dir.length; i++) {
-    if (dir[i] === "..") {
-      url.pop();
-    } else if (dir[i] !== ".") {
-      url.push(dir[i]);
-    }
-  }
-  return url.join("/");
+  return new URL(path, meta.url).href;
 }
 
 export async function wasm<Mod extends Record<string, unknown>>(
@@ -18,4 +9,13 @@ export async function wasm<Mod extends Record<string, unknown>>(
   const stream = fetch(resolve(meta, path));
   const mod = await WebAssembly.instantiateStreaming(stream);
   return mod.instance.exports as Mod;
+}
+
+export function worker<Mod extends Record<string, unknown>>(
+  meta: ImportMeta,
+  path: string
+) {
+  return new Worker(resolve(meta, path), {
+    type: "module",
+  });
 }
